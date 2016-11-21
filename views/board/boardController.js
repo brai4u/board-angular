c5c08a0d55d57fb8df7196240407785448f958b2
Brian Ardiles <ardiles.brian@gmail.com>
2016-11-21 15:43:36 -0300

    creacion del boardcontroller con foreach, modificacion del index

--- views/board/boardController.js
+++ views/board/boardController.js
@@ -1,1 +1,9 @@
-asd
+var app = angular.module('boardApp', []);
+app.controller('boardController', function($scope, $http, $filter) {
+    $http.get("http://private-52da20-board4.apiary-mock.com/issues").then(function (response) {
+	
+		angular.forEach(response.data, function(issues) {
+			$scope.issues = issues.issues;
+		});
+    });
+});

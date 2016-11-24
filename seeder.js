fcc93c3ed7b2817ae5be63e3a6f9a8484f3004fc
Brian Ardiles <ardiles.brian@gmail.com>
2016-11-24 14:40:01 -0300

    modifacion del seeder para poder usar en la db board

--- seeder.js
+++ seeder.js
@@ -1,44 +1,40 @@
-			/*db.get('number').then(function (doc) {
-			  doc.status = 'tees';
-			  return db.put(doc);
-			});*/
-
-			/*var doc = {
-			  "_id": "2",
-			  "title": "el scroll se esconde debajo del footer",
-			  "description": "esta es la descripcion",
-			  "status": "to-do"
-			};
+var db = new PouchDB('http://localhost:5984/board');
+var doc = {
+	  "_id": "2",
+	  "title": "el scroll se esconde debajo del footer",
+	  "description": "esta es la descripcion",
+	  "status": "to-do"
+	};
 
 
-			db.put(doc);
+	db.put(doc);
 
-			var doc2 = {
-			  "_id": "25",
-			  "title": "En 1280 x 800 se buguea el div rojo",
-			  "description": "esta es la descripcion",
-			  "status": "to-do"
-			};
-
-
-			db.put(doc2);
-
-			var doc3 = {
-			  "_id": "5",
-			  "title": "ajustar los margenes de todos div",
-			  "description": "esta es la descripcion",
-			  "status": "in-progress"
-			};
+	var doc2 = {
+	  "_id": "25",
+	  "title": "En 1280 x 800 se buguea el div rojo",
+	  "description": "esta es la descripcion",
+	  "status": "to-do"
+	};
 
 
-			db.put(doc3);
+	db.put(doc2);
 
-			var doc4 = {
-			  "_id": "15",
-			  "title": "No anda nada",
-			  "description": "esta es la descripcion",
-			  "status": "done"
-			};
+	var doc3 = {
+	  "_id": "5",
+	  "title": "ajustar los margenes de todos div",
+	  "description": "esta es la descripcion",
+	  "status": "in-progress"
+	};
 
 
-			db.put(doc4);*/
+	db.put(doc3);
+
+	var doc4 = {
+	  "_id": "15",
+	  "title": "No anda nada",
+	  "description": "esta es la descripcion",
+	  "status": "done"
+	};
+
+
+	db.put(doc4);
